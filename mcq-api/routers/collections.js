31b8a329d49c55721f50dc5d20a1ae97960fcf60
AchinthaHarshamal <achinthafharshamal@gmail.com>
2023-09-01 23:35:57 +0530

    get collections anad a collection

--- mcq-api/routers/collections.js
+++ mcq-api/routers/collections.js
@@ -6,30 +6,61 @@ const router = express.Router();
 
 
 router.get('/collections', (req, res) => {
-    console.log('I am /collections');
-    res.send("collection")
+    collectionSchema.find()
+        .then(collections => {
+            res.status(200).json(collections);
+            // console.log(res);
+        }).catch(err => {
+            // console.log(err);
+            res.status(500).json({ error: 'Internal server error' });
+        })
+
 })
 
-router.get('/collections/:id', (req, res) => {
-    console.log(req.params);
-    res.send(`collection ${req.params.id}`)
+router.get('/collections/:id', async (req, res) => {
+
+    try {
+        const collectionId = req.params.id;
+
+        // receive the collection by id 
+        const collection = await collectionSchema.findById(collectionId);
+
+        if (!collection) {
+            return res.status(404).json({ error: 'Collection not found' });
+        }
+
+        const mcqIds = collection.qIds;
+
+        const mcqs = await mcqSchema.find({ _id: { $in: mcqIds } });
+
+        const collectionWithMCQs = {
+            _id: collection._id,
+            title: collection.title,
+            author: collection.author,
+            mcqs: mcqs,
+        };
+
+        res.status(200).json(collectionWithMCQs);
+    } catch (err) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+
 })
+
 
 
 router.post('/collection/insert', async (req, res) => {
-    //console.log(req.body.mcq[2]);
 
     try {
-        const { title, author, mcqs} = req.body; 
-        const  nQs = mcqs.length;
-        
+        const { title, author, mcqs } = req.body;
+        const nQs = mcqs.length;
+
         const mcqIds = [];
-        for(const mcq of mcqs  ){
-            const mcqS = new mcqSchema({...mcq});
+        for (const mcq of mcqs) {
+            const mcqS = new mcqSchema({ ...mcq });
             const savedMcq = await mcqS.save();
             mcqIds.push(savedMcq._id);
         }
-
 
         const collection = new collectionSchema({
             collectionId: 5,
@@ -39,13 +70,12 @@ router.post('/collection/insert', async (req, res) => {
             qIds: mcqIds
         });
 
-        await collection.save(); 
+        await collection.save();
         res.status(201).json({ message: 'Data received and saved successfully', mcqIds });
-    }catch(err) {
+    } catch (err) {
+        console.log(err);
         res.status(500).json({ error: 'Internal server error' });
     }
 
-    
-    
 })
 module.exports = router
